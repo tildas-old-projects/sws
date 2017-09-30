4ae6b90d19e0782b219a40ff16d75dbc140bab03
S Stewart <iamtheworst@programmer.net>
2017-09-29 20:44:45 -0500

    examples[test[doc]]: add shit

--- examples/test.js
+++ examples/test.js
@@ -1,3 +1,9 @@
+/**
+ * A simple demo of sws
+ * @copyright S Stewart, 2017
+ * @license MIT
+ * @file Example file
+ */
 const sws = require('../index')
 
 console.log('Hey hey! This is a demo of sws, a simple web server')
